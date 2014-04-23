73d16c87144cb77e38a923464af87ca7c203df66
Justin Halsall <justin@juice10.com>
2014-04-23 19:58:00 +0200

    ugly way of allowing multiple domains

--- app/controllers/trips/update.js
+++ app/controllers/trips/update.js
@@ -9,17 +9,21 @@ var songs = [
 
 var cur = 0;
 
-module.exports = function (req, res, next) {  
+module.exports = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
   if (true !== req.body.new) {
     res.send({
       success: true
-    });  
+    });
   } else {
     res.send({
       success: true,
       songID: songs[cur]
     });
-    
+
     if (++cur >= songs.length) cur = 0;
   }
 };
